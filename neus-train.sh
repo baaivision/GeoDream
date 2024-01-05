91787979fd7d79807936640b50291fa894dfaa01
mabaorui <805950246@qq.com>
2024-01-05 20:26:58 +0800

    branch-threestudio

--- neus-train.sh
+++ neus-train.sh
@@ -9,4 +9,4 @@ echo "$file_contents"
 vol_dir=$file_dir"con_volume_lod_150.pth"
 echo "$vol_dir"
 
-python -u launch.py --config custom/threestudio-geodream/configs/geodream-neus.yaml --train --gpu 1 system.prompt_processor.prompt="$file_contents" system.geometry.init_volume_path="$vol_dir"
+python -u launch.py --config custom/threestudio-geodream/configs/geodream-neus.yaml --train --gpu 0 system.prompt_processor.prompt="$file_contents" system.geometry.init_volume_path="$vol_dir"
